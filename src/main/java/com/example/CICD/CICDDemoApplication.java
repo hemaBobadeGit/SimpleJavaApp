9624ac262ca0d347162de30904fff51453e2d20b
Hema <hemabobadekar@outlook.com>
2025-03-13 11:20:16 -0700

    Initial commit

---
+++ src/main/java/com/example/CICD/CICDDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.CICD;
+
+
+
+	public class CICDDemoApplication {
+
+	   
+
+	    public static void main(String[] args) {
+	    	 System.out.println("Hello, CI/CD!");
+	    }
+
+	   
+}
---
+++ src/test/java/com/example/CICD/CICDApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.CICD;
+
+	import static org.junit.Assert.*;
+
+	import org.junit.Test;
+
+	public class CICDApplicationTests {
+	    @Test
+	    public void testOutput() {
+	        assertEquals("Hello, CI/CD!", "Hello, CI/CD!");
+	    }
+	}
